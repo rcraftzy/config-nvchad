11d386750f2794bc33a14953cc0e798a18d34010
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-09-09 14:31:33 -0500

    fix: Change the override for the nvChad update

--- chadrc.lua
+++ chadrc.lua
@@ -2,45 +2,23 @@
 
 local M = {}
 
+local highlights = require "custom.highlights"
 -- make sure you maintain the structure of `core/default_config.lua` here,
 -- example of changing theme:
 
 M.ui = {
-  theme = "tomorrow_night",
+  theme = "chadracula",
+  hl_override = highlights,
+  transparency = true,
 }
 
 local pluginConfs = require "custom.plugins.configs"
 
-M.plugins ={
+M.plugins = {
   user = require "custom.plugins",
-  override = {
-    ["neovim/nvim-lspconfig"] = {
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.plugins.lspconfig"
-      end,
-    },
-    ["williamboman/mason.nvim"] = {
-      ensure_installed = {
-        -- lua stuff
-        "lua-language-server",
-        "stylua",
-
-        -- web dev
-        "css-lsp",
-        "html-lsp",
-        "typescript-language-server",
-        "deno",
-        "emmet-ls",
-        "json-lsp",
-
-        -- shell
-        "shfmt",
-        "shellcheck",
-      },
-    },
-    ["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
-  }
+  ["williamboman/mason.nvim"] = pluginConfs.mason,
+  ["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
+  ["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
 }
 
 M.mappings = require "custom.mappings"
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,8 +1,15 @@
 return {
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
   ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
     config = function()
       require "custom.plugins.null-ls"
     end,
-  }
+  },
+  ["p00f/nvim-ts-rainbow"] = {}
 }
