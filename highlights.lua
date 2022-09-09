79d93988d18f6711659f26a6e628644092475e6a
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-09-09 14:33:27 -0500

    feat: Add highlights for have words in Italic

---
+++ highlights.lua
@@ -0,0 +1,11 @@
+return {
+  Comment = { italic = true },
+  Function = { italic = true },
+  Type = { italic = true },
+  SpecialComment = { italic = true },
+  TSAttribute = { italic = true },
+  TSParameter = { italic = true },
+  TSLabel = { italic = true },
+  TSVariableBuiltin = { italic = true },
+  TSTagAttribute = { italic = true },
+}
