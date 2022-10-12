e247f7e8b23a03b385024e65088fa5c459dd6678
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-10-12 17:11:44 -0500

    feat: Create new fucntion on_attach for formatt on save for gopls

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "clangd", "tsserver", "tailwindcss"}
+local servers = { "html", "cssls", "tsserver", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -14,9 +14,23 @@ end
 
 local util = require "lspconfig/util"
 
+local attach = function(client, bufnr)
+  -- format on save
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.formatting_seq_sync()
+      end,
+    })
+  end
+end
+
 lspconfig.gopls.setup {
-  cmd = {"gopls", "serve"},
-  filetypes = {"go", "gomod"},
+  on_attach = attach,
+  cmd = { "gopls", "serve" },
+  filetypes = { "go", "gomod" },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
@@ -27,25 +41,20 @@ lspconfig.gopls.setup {
     },
   },
 }
-
 lspconfig.denols.setup {
-  -- Omitting some options
-  root_dir = util.root_pattern("deno.json"),
-
+  root_dir = util.root_pattern "deno.json",
 }
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
+lspconfig.emmet_ls.setup {
+  capabilities = capabilities,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+  init_options = {
+    html = {
+      options = {
+        ["bem.enabled"] = true,
       },
-    }
-})
+    },
+  },
+}
 -- lspconfig.volar.setup{
 --   filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
 -- }
