f0855e938c4f88a5b60b11fd521de23eb975386a
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-10-22 10:28:43 -0500

    :sparkles: Add new mappings for plugin the testing rest api in nvim

--- mappings.lua
+++ mappings.lua
@@ -3,24 +3,29 @@ local M = {}
 -- add this table only when you want to disable default keys
 M.disabled = {
   n = {
-      ["<leader>q"] = "",
-      ["<C-s>"] = ""
-  }
+    ["<leader>q"] = "",
+    ["<C-s>"] = "",
+  },
 }
 
 M.abc = {
 
   n = {
-     ["<leader>w"] = { "<cmd> w <CR>", "save file" },
-     ["<leader>q"] = { "<cmd> q <CR>", "save file" },
-     ["<C-s>"] = { "<cmd> w! <CR>", "save file" },
+    ["<leader>w"] = { "<cmd> w <CR>", "save file" },
+    ["<leader>q"] = { "<cmd> q <CR>", "save file" },
+    ["<C-q>"] = { "<cmd> q! <CR>", "save file" },
+    ["<C-s>"] = { "<cmd> w! <CR>", "save file" },
 
+    ["<leader>rr"] = {"<cmd> lua require ('rest-nvim').run() <CR>", opts = { noremap = true } },
+    ["<leader>rl"] = { "<cmd> lua require ('rest-nvim').last() <CR>", opts = { noremap = true } },
+    ["<leader>rp"] = { "<cmd> lua require ('rest-nvim').run(true) <CR>", opts = { noremap = true } },
   },
 
   i = {
     -- more keys!
-  }
+  },
 }
+M.rest = {}
 
 M.xyz = {
   -- stuff
