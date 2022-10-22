5bf0c6b1c233a0710971b9356f13ac69f391fa97
roberto-carlos-tg <roberto.tg99@gmail.com>
2022-10-22 10:33:01 -0500

    :recycle: Refactor the init plugins custom

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,15 +1,29 @@
+local pluginConfs = require "custom.plugins.configs"
+
 return {
+  -- Override config
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
     end,
   },
+  ["williamboman/mason.nvim"] = pluginConfs.mason,
+  ["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
+  ["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
+
+  -- New pluggins
   ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
     config = function()
       require "custom.plugins.null-ls"
     end,
   },
-  ["p00f/nvim-ts-rainbow"] = {}
+  ["p00f/nvim-ts-rainbow"] = {},
+  ["NTBBloodbath/rest.nvim"] = {
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      require "custom.plugins.rest"
+    end,
+  },
 }
